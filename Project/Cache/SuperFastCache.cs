cc4799fa5695e43eb0731bf81553786283ec045c
Vicsante Tangpos Aseniero <vaseniero@gmail.com>
2022-11-06 18:42:59 +0800

    Added the 'answer for the one optional task' and also changes in variable name from 'Dict' to 'Items'

--- Project/Cache/SuperFastCache.cs
+++ Project/Cache/SuperFastCache.cs
@@ -10,7 +10,7 @@ namespace Diana.Code.Challenge
  public sealed class SuperFastCache<T> : iCacheStuff<T> where T : employee
  {
   private static readonly SuperFastCache<T> instance = new SuperFastCache<T>(); // Non-lazy or Eager loading Singleton Design Pattern
-  public static Dictionary<Guid, T> Dict { get; private set; } = new Dictionary<Guid, T>();
+  internal static Dictionary<Guid, T> Items { get; private set; } = new Dictionary<Guid, T>();
   public string CacheName => "Super Fast Cache x1000 faster!\r\n\r\nTODO: Please Implement this class and make it super fast.\r\n";
 
   private SuperFastCache()
@@ -28,7 +28,7 @@ namespace Diana.Code.Challenge
   public void AddItem(T newItem)
   {
    //throw new NotImplementedException("Please implement the AddItem method.");
-   Dict.Add(newItem.Id, newItem);
+   Items.Add(newItem.Id, newItem);
   }
 
   public string GetName(Guid id)
@@ -40,7 +40,7 @@ namespace Diana.Code.Challenge
   public T FindItem(Guid id)
   {
    //throw new NotImplementedException("Please implement the FindItem method.");
-   return Dict[id];
+   return Items[id];
   }
  }
 }
--- Project/Models/Company.cs
+++ Project/Models/Company.cs
@@ -4,36 +4,36 @@ using Diana.Code.Challenge;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 
-namespace Diana.Code.Chaallenge
+namespace Diana.Code.Challenge
 {
-    /// <summary>
-    /// Company class that contains information about the company and its employees.
-    /// </summary>
-    public class Company : iCachedObject
-    {
-        [BsonId]
-        public Guid Id { get; set; }
+ /// <summary>
+ /// Company class that contains information about the company and its employees.
+ /// </summary>
+ public class Company : iCachedObject
+ {
+  [BsonId]
+  public Guid Id { get; set; }
 
-        public string name { get; set; }
+  public string name { get; set; }
 
-        public string Description { get; set; }
+  public string Description { get; set; }
 
-        public Decimal Profit_1 { get; set; }
+  public Decimal Profit_1 { get; set; }
 
-        public Double Profit_2 { get; set; }
+  public Double Profit_2 { get; set; }
 
-        /// <question>
-        /// What issues can you see with using string as the key for the dictionary?
-        /// What suggestions do you have to resolve this?
-        /// </question>
-        /// <answer>
-        ///
-        /// </answer>
-        public Dictionary<string, employee> Employees { get; set; } = new Dictionary<string, employee>();
+  /// <question>
+  /// What issues can you see with using string as the key for the dictionary?
+  /// What suggestions do you have to resolve this?
+  /// </question>
+  /// <answer>
+  /// It would be a better approach to use "Guid" instead of "string" as a key since it will be unique i.e Guid.NewGuid()
+  /// </answer>
+  public Dictionary<string, employee> Employees { get; set; } = new Dictionary<string, employee>();
 
-        public int CompareTo(object obj)
-        {
-            return this.Id.CompareTo(((employee)obj).Id);
-        }
-    }
+  public int CompareTo(object obj)
+  {
+   return this.Id.CompareTo(((employee)obj).Id);
+  }
+ }
 }
